b61de3a1113f1e730dc1585197858796b6a47c0a
GyuHyeong Nam <sjarbgud@gmail.com>
2022-08-24 20:53:42 +0900

    05. State

--- React 공식문서로 디테일잡기(초급)/my-app/src/App.js
+++ React 공식문서로 디테일잡기(초급)/my-app/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Compositon from './components/2-4.props/Compositon';
 import Extration from './components/2-4.props/Extraction/Extration';
+import ClassComponent from './components/2-5.State/ClassComponent';
+import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
-      <Compositon />
+      <FunctionalComponent />
+      <ClassComponent />
       <Extration />
+      <Compositon />
     </div>
   );
 }
