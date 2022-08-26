d1c9edcdac67ce9fecdf4609c8be2349d8375738
GyuHyeong Nam <sjarbgud@gmail.com>
2022-08-26 23:53:31 +0900

    07. 이벤트

--- React 공식문서로 디테일잡기(초급)/my-app/src/App.js
+++ React 공식문서로 디테일잡기(초급)/my-app/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import Compositon from './components/2-4.props/Compositon';
-import Extration from './components/2-4.props/Extraction/Extration';
-import ClassComponent from './components/2-5.State/ClassComponent';
-import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
-import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+import Event from './components/2-7.Event/Event';
+// import Compositon from './components/2-4.props/Compositon';
+// import Extration from './components/2-4.props/Extraction/Extration';
+// import ClassComponent from './components/2-5.State/ClassComponent';
+// import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
+// import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
-      <ClassComponent2 />
+      <Event />
+      {/* <ClassComponent2 /> */}
       {/* <FunctionalComponent /> */}
       {/* <ClassComponent /> */}
       {/* <Extration /> */}
